4acd0fae3c7531464b2ace5a4afc92ac03602c07
Sarah Zajac <sarahmzajac@gmail.com>
2019-07-09 19:17:28 +0000

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,1 +1,1 @@
-aws s3 sync build/ s3://{profiles.kenzie.academy} --exclude ".DS_Store/*" --cache-control "max-age=120000" --delete
+aws s3 sync build/ s3://profiles.kenzie.academy --exclude ".DS_Store/*" --cache-control "max-age=120000" --delete
