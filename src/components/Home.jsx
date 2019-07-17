2aa69305be745f72045cc61d1e74d197eca97839
Sam Kane <samuelwkane@gmail.com>
2019-07-17 18:26:58 +0000

    Ko 353

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,35 +1,38 @@
 import React from "react";
-import CoverImage from '../assets/cover.jpg'
-import { makeStyles } from '@material-ui/styles';
+import CoverImage from "../assets/cover.jpg";
+import { makeStyles } from "@material-ui/styles";
+import SideBar from "./SideBar";
 
 const useStyles = makeStyles(theme => ({
-    root: {
-        backgroundImage: `url(${CoverImage})`,
-        padding: '200px 0px',
-        color: theme.palette.primary.white,
-        backgroundSize: '100%',
-        backgroundPosition: '0px 20%',
-        backgroundRepeat: 'no-repeat',
-        opacity: '0.8',
-        textShadow: '0px 1px 2px #616368',
-        '& .header':{
-            fontSize: '60px',
-            fontWeight: 'bold',
-            margin: '5px',
-        },
-        '& .subHeader':{
-            fontSize: '30px',
-            margin: '5px',
-        },
+  root: {
+    backgroundImage: `url(${CoverImage})`,
+    padding: "200px 0px",
+    color: theme.palette.primary.white,
+    backgroundSize: "100%",
+    backgroundPosition: "0px 20%",
+    backgroundRepeat: "no-repeat",
+    textShadow: "0px 1px 2px #616368",
+    "& .header": {
+      fontSize: "60px",
+      fontWeight: "bold",
+      margin: "5px"
+    },
+    "& .subHeader": {
+      fontSize: "30px",
+      margin: "5px"
     }
-}))
+  }
+}));
 
-export default function Home(){
-    const classes = useStyles();
-    return(
-        <div className={classes.root}>
-        <div className='header'>Kenzie Candidates</div>
-        <div className='subHeader'>Make your next hire a Kenzie graduate.</div>
-        </div>
-    )
+export default function Home() {
+  const classes = useStyles();
+  return (
+    <React.Fragment>
+      <div className={classes.root}>
+        <div className="header">Kenzie Candidates</div>
+        <div className="subHeader">Make your next hire a Kenzie graduate.</div>
+      </div>
+      <SideBar />
+    </React.Fragment>
+  );
 }
