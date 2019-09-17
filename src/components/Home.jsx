d0f3323967742c2f12fedba9db72a63a2719aa54
Sam Kane <samuelwkane@gmail.com>
2019-09-17 20:27:23 +0000

    List students from Classe

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,26 +1,15 @@
 import React from "react";
-import CoverImage from "../assets/cover.jpg";
 import { makeStyles } from "@material-ui/styles";
 import SideBar from "./SideBar";
+import StudentCardList from './StudentCardList';
+import Hero from './Hero';
 
 const useStyles = makeStyles(theme => ({
   root: {
-    backgroundImage: `url(${CoverImage})`,
-    padding: "200px 0px",
-    color: theme.palette.primary.white,
-    backgroundSize: "100%",
-    backgroundPosition: "0px 20%",
-    backgroundRepeat: "no-repeat",
-    textShadow: "0px 1px 2px #616368",
-    "& .header": {
-      fontSize: "60px",
-      fontWeight: "bold",
-      margin: "5px"
+    "& .content":{
+      display: 'flex',
+      flexDirection: 'row'
     },
-    "& .subHeader": {
-      fontSize: "30px",
-      margin: "5px"
-    }
   }
 }));
 
@@ -29,10 +18,12 @@ export default function Home() {
   return (
     <React.Fragment>
       <div className={classes.root}>
-        <div className="header">Kenzie Candidates</div>
-        <div className="subHeader">Make your next hire a Kenzie graduate.</div>
+        <Hero/>
+        <div className="content">
+          <SideBar />
+          <StudentCardList/>
+        </div>
       </div>
-      <SideBar />
     </React.Fragment>
   );
 }
